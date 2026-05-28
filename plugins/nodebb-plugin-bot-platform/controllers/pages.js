60f5c27bb3f7d31ede7ded802401f08bbff55b55
aeolusnee <fengni@gmail.com>
2026-05-28 15:59:39 +0800

    fix: 修复管理员私信/私群页面 isAdmin 判断
    req.user.isAdmin 不存在于 NodeBB 的 user session 对象，
    改用 user.isAdministrator(uid) 正确判断管理员身份。
    之前管理员看到的是 owner 视图而非 admin 视图。

--- plugins/nodebb-plugin-bot-platform/controllers/pages.js
+++ plugins/nodebb-plugin-bot-platform/controllers/pages.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const user = require('../../../src/user');
+
 exports.manageBots = async function (req, res) {
 	res.render('bots/manage', {
 		title: 'Bot 管理',
@@ -8,17 +10,19 @@ exports.manageBots = async function (req, res) {
 };
 
 exports.pmMonitor = async function (req, res) {
+	const isAdmin = req.uid ? await user.isAdministrator(req.uid) : false;
 	res.render('bots/pm-monitor', {
 		title: 'Bot 私信记录',
 		uid: req.uid,
-		isAdmin: !!(req.user && req.user.isAdmin),
+		isAdmin: isAdmin,
 	});
 };
 
 exports.groupMonitor = async function (req, res) {
+	const isAdmin = req.uid ? await user.isAdministrator(req.uid) : false;
 	res.render('bots/group-monitor', {
 		title: 'Bot 私群记录',
 		uid: req.uid,
-		isAdmin: !!(req.user && req.user.isAdmin),
+		isAdmin: isAdmin,
 	});
 };
